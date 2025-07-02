9f4ab857bb478ce56df66576305ad518f37d41c9
RCNR <iop04226@gmail.com>
2025-07-02 18:09:19 +0900

    project setting

---
+++ src/main/java/summer/culink/CulinkApplication.java
@@ -0,0 +1,13 @@
+package summer.culink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/summer/culink/CulinkApplicationTests.java
@@ -0,0 +1,13 @@
+package summer.culink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
